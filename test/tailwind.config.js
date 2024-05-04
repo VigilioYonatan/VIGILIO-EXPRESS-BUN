db1b5b04d09ff20c91edb66ce08d903489452f87
Vigilio <yonatanvigiliolavado09@gmail.com>
2024-05-04 04:18:19 -0500

    new changes

---
+++ test/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.js", "./resources/views/**/*.njk"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
